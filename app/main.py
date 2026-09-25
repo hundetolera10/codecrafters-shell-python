1fee88058b345f397527eb04a6a42483969b2eff
Hunde Tolera <hundetolera225@gmail>
2026-09-25 18:19:43 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ def main():
         elif command.startswith("echo "):
             print(command[5:])
         elif command[5:] in ["type","echo","exit"]:
-            print(f"{command[5:]}: is a shell builtin")
-        else:
+            print(f"{command[5:]} is a shell builtin")
+        else: 
             print(f"{command.strip('type')}: not found")
     
 if __name__ == "__main__":
