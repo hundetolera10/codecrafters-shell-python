73f7a9c602c316ea7e408f060e222eb69a07e174
Hunde Tolera <hundetolera225@gmail.com>
2026-09-25 20:10:06 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,30 +1,73 @@
 import sys
-import shutil
+import os
+import subprocess
+
+
+BUILTIN_COMMANDS = ["type", "echo", "exit"]
+PATH_DIRECTORIES = os.environ.get("PATH", "").split(os.pathsep)
+
+
+def is_executable(file_path):
+    return os.access(file_path, os.X_OK)
+
+
+def is_file(file_path):
+    return os.path.isfile(file_path)
+
+
+def get_file_path(command):
+    for directory in PATH_DIRECTORIES:
+        file_path = os.path.join(directory, command)
+
+        if is_file(file_path) and is_executable(file_path):
+            return file_path
+
+    return None
+
+
+def print_command_type(command):
+    if command in BUILTIN_COMMANDS:
+        print(f"{command} is a shell builtin")
+    else:
+        file_path = get_file_path(command)
+
+        if file_path is not None:
+            print(f"{command} is {file_path}")
+        else:
+            print(f"{command}: not found")
 
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input()
+
+        user_input = input()
+        parts = user_input.split()
+
+        if not parts:
+            continue
+
+        command = parts[0]
+        arguments = parts[1:]
 
         if command == "exit":
             break
 
-        elif command.startswith("echo "):
-            print(command[5:])
+        elif command == "echo":
+            print(" ".join(arguments))
 
-        elif command.startswith("type "):
-            target = command[5:]
-
-            if target in ["type", "echo", "exit"]:
-                print(f"{target} is a shell builtin")
-            elif path := shutil.which(target):
-                print(f"{target} is {path}")
-            else:
-                print(f"{target}: not found")
+        elif command == "type":
+            print_command_type(" ".join(arguments))
 
         else:
-            print(f"{command}: command not found")
+            file_path = get_file_path(command)
+
+            if file_path is not None:
+                arguments.insert(0, command)
+                subprocess.run(arguments, executable=file_path)
+
+            else:
+                print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
