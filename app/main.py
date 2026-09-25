621dd9bb080b56a5f637a6c0d3ebdf0e879f924e
Hunde Tolera <hundetolera225@gmail>
2026-09-25 18:09:15 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ def main():
         elif command.startswith("echo "):
             print(command[5:])
         elif command[5:] in ["type","echo","exit"]:
-            print(f"{command[5:]}: command found")
+            print(f"{command[5:]}: is a shell builtin")
         else:
             print(f"{command.strip('type')}: not found")
     
