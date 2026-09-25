1b9b97ad5b1eb365e4d8bb1eaae579a4f1f524c3
Hunde Tolera <hundetolera225@gmail>
2026-09-25 16:04:15 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,8 +6,11 @@ def main():
         sys.stdout.write("$ ")
         command = input()
         if command == "exit":
-            break   
-        print(f"{command}: command not found")
+            break
+        elif command.startswith("echo "):
+            print(command[5:])
+        else:
+            print(f"{command}: command not found")
     
 if __name__ == "__main__":
     main()
